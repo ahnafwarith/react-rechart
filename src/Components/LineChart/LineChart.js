e3a783a6ae861b6eb8da232d3a6ddcd269ed38d2
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-03-29 23:32:04 +0600

    initialized for making chart with rechart

---
+++ src/Components/LineChart/LineChart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LineChart = () => {
+    return (
+        <div>
+            <h1>This is a line chart</h1>
+        </div>
+    );
+};
+
+export default LineChart;
